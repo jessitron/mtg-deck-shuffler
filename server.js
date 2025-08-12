83e1f5cc28781a1f9e6978fe7f1a024f629d0d0f
Jessica Kerr <jessitron@gmail.com>
2025-08-11 20:51:28 -0500

    Add deck input functionality with htmx
    - Add htmx script tag to index.html
    - Create input field for archidekt deck number
    - Add POST /deck endpoint that returns HTML response
    - Set up htmx to replace parent div with response
    
    - claude

--- server.js
+++ server.js
@@ -4,7 +4,16 @@ const path = require('path');
 const app = express();
 const PORT = process.env.PORT || 3000;
 
+app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname)));
+
+app.post('/deck', (req, res) => {
+    const deckNumber = req.body['deck-number'];
+    res.send(`<div id="deck-input">
+        <p>You have chosen deck ${deckNumber}</p>
+        <a href="/">Choose another deck</a>
+    </div>`);
+});
 
 app.listen(PORT, () => {
     console.log(`Server running on http://localhost:${PORT}`);
