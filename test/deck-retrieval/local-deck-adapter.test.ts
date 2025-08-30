588a22db57d86544a6ca2d8145e3dd6c585651c3
Jessica Kerr <jessitron@gmail.com>
2025-08-30 08:35:22 -0500

    The test running is screwy
    if we specify test/**/*.test.ts then it misses test/GameState.test.ts, even though an ls with that path picks it up. But if we don't specify the path at all, it runs it.

--- test/deck-retrieval/local-deck-adapter.test.ts
+++ test/deck-retrieval/local-deck-adapter.test.ts
@@ -2,7 +2,7 @@ import { test, describe } from "node:test";
 import assert from "node:assert";
 import { LocalDeckAdapter } from "../../src/port-deck-retrieval/localAdapter/LocalDeckAdapter.js";
 
-describe("ArchidektDeckToDeckAdapter", () => {
+describe("LocalDeckAdapter", () => {
   let adapter: LocalDeckAdapter = new LocalDeckAdapter();
 
   test("reads files from the local directory", async () => {
