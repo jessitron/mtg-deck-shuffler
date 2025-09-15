67224a0633a893db47d1622ac84941f0df909767
Jessica Kerr <jessitron@gmail.com>
2025-09-15 15:55:17 -0500

    Small is a little too small, unclear text

--- src/view/common/shared-components.ts
+++ src/view/common/shared-components.ts
@@ -12,13 +12,15 @@ export function formatCommanderImageHtmlFragment(commanders: any[]): string {
     ? `<div class="commander-placeholder">No Commander</div>`
     : `<div id="command-zone">
           ${commanders
-            .map((commander) => `<img src="${getCardImageUrl(commander.scryfallId, "small")}" alt="${commander.name}" class="mtg-card-image commander-image" />`)
+            .map(
+              (commander) => `<img src="${getCardImageUrl(commander.scryfallId, "normal")}" alt="${commander.name}" class="mtg-card-image commander-image" />`
+            )
             .join("")}
         </div>`;
 }
 
 export function formatCardContainerHtmlFragment(gameCard: GameCard, containerType: "revealed" | "hand", actions: string, animationClass = ""): string {
-  const imageUrl = getCardImageUrl(gameCard.card.scryfallId, "small");
+  const imageUrl = getCardImageUrl(gameCard.card.scryfallId, "normal");
   const cardClass = containerType === "revealed" ? "revealed-card" : "hand-card";
 
   return `<div id="card-container-${gameCard.gameCardIndex}" class="${containerType}-card-container">
