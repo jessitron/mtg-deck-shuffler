a3b47270980e1b0b8ce9d07de61c4c1ca0ffcd40
Jessica Kerr <jessitron@gmail.com>
2025-12-24 18:43:53 -0600

    tweaks

--- src/view/debug/debug-section.ts
+++ src/view/debug/debug-section.ts
@@ -1,6 +1,10 @@
 
 export function formatDebugSectionHtmlFragment(gameId: number, stateVersion: number): string {
-  return `<p class="game-id">Game ID: ${gameId} | State Version: ${stateVersion}</p>
+  return `<p class="game-id">Game ID: ${gameId} | <button class="debug-button"
+                  hx-get="/debug-state/${gameId}"
+                  hx-target="#modal-container"
+                  hx-swap="innerHTML"
+                  class="debug-button">State</button>: ${stateVersion}</p>
   <p class="browser-tab-id">Tab ID: <span id="browser-tab-id-display">loading...</span></p>
   <script>
     // Display the browser tab ID once available
@@ -9,9 +13,5 @@ export function formatDebugSectionHtmlFragment(gameId: number, stateVersion: num
       document.getElementById('browser-tab-id-display').title = window.browserTabId;
     }
   </script>
-  <button class="debug-button"
-                  hx-get="/debug-state/${gameId}"
-                  hx-target="#modal-container"
-                  hx-swap="innerHTML"
-                  class="debug-button">Debug State</button>`;
+  `;
 }
