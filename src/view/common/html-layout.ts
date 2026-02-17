d1ba5189edcb5cad429aabf5ed552e7a6c2f238d
Jessica Kerr <jessitron@gmail.com>
2026-02-17 10:58:52 -0600

    Add Orbitron font to game page for card modal titles - auggie

--- src/view/common/html-layout.ts
+++ src/view/common/html-layout.ts
@@ -7,7 +7,7 @@ function formatHtmlHead(title: string, additionalStylesheets: string[] = []): st
     <title>${title}</title>
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-    <link href="https://fonts.googleapis.com/css2?family=Inter:ital,opsz,wght@0,14..32,100..900;1,14..32,100..900&family=Rampart+One&display=swap" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css2?family=Inter:ital,opsz,wght@0,14..32,100..900;1,14..32,100..900&family=Rampart+One&family=Orbitron:wght@400;600;700;900&display=swap" rel="stylesheet">
     <link rel="stylesheet" href="/styles.css" />
     <link rel="stylesheet" href="/game.css" />
     <link rel="stylesheet" href="/playmat.css" />
