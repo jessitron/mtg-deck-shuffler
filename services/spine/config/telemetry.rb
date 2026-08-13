9e478c3afe28417a4c42a41543c2da5871411256
Jessica Kerr <jessitron@gmail.com>
2026-08-13 09:16:58 -0500

    Try to custom-sample down the /spine/up spans

--- services/spine/config/telemetry.rb
+++ services/spine/config/telemetry.rb
@@ -7,3 +7,22 @@ OpenTelemetry::SDK.configure do |c|
   c.service_name = ENV.fetch("OTEL_SERVICE_NAME", "mtg-spine")
   c.use "OpenTelemetry::Instrumentation::Rack"
 end
+
+class KeepItDownSampler
+  def should_sample?(trace_id:, parent_context:, links:, name:, kind:, attributes:)
+    if attributes && attributes["url.path"] == "/spine/up"
+      sample_rate = 0.01;
+      
+    else
+      sample_rate = 1.0;
+    end
+    attributes["sample_rate"] = sample_rate
+    return  OpenTelemetry::SDK::Trace::Samplers.trace_id_ratio_based(sample_rate).should_sample?(trace_id: trace_id, parent_context: parent_context, links: links, name: name, kind: kind, attributes: attributes)
+  end
+
+  def description
+    "KeepItDownSampler (1% of /spine/up, 100% of everything else)"
+  end
+end
+
+OpenTelemetry.tracer_provider.sampler = KeepItDownSampler.new()
